7d5f4084be522d1efbbb30c09afeb77cd4964452
Jack.Respberry.Ran <zhen.dong.1234@163.com>
2020-07-17 01:15:21 +0800

    Add GPIO code

--- flaskr/respberry.py
+++ flaskr/respberry.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import paho.mqtt.client as mqtt
+import json
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -9,3 +12,42 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+Host = "emqx.eguagua.cn"
+Port = 1883
+bedroom = 35
+ch = 26
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(bedroom, GPIO.OUT)
+GPIO.setup(ch, GPIO.OUT)
+
+def opens():
+    print('|      Open Light !      |')
+    print('+------------------------+')
+    GPIO.output(ch, GPIO.LOW)
+
+def close():
+    print('|      Down Light !      |')
+    print('+------------------------+')
+    GPIO.output(ch, GPIO.HIGH)
+
+def on_connect(client, userdata, flags, rc):
+    print("emqx.eguagua.cn Connected with result code "+str(rc))
+    client.subscribe("zhendong")
+
+def on_message(client, userdata, msg):
+    code = msg.payload.decode('utf-8')
+    print("\nGet The Infomation!:\n+------------------------+")
+    if code == '开灯':
+        opens()
+    elif  code == '关灯':
+        close()
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(username='zhendong', password='zhendong')
+    client.connect(Host, Port, 300)
+    client.loop_forever()
