a79bdb5349b188a07c7ea13f6e3314fcf2449702
Jack.Respberry.Ran <zhen.dong.1234@163.com>
2020-07-17 00:25:29 +0800

    Start flask project with gunicorn

---
+++ flaskr/config.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+bind = '127.0.0.1:8001'
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
+backlog = 2048
+worker_class = "gevent"
+worker_connections = 1000
+daemon = False
+debug = True
+proc_name = 'respberry'
+# pidfile = './log/gunicorn.pid'
+# errorlog = './log/gunicorn.log'
