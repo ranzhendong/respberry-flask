6296211bdf44f10113e473c5e4ef24e59ae6b086
Jack.Respberry.Ran <zhen.dong.1234@163.com>
2020-07-20 23:58:35 +0800

    Update respberry.py

--- flaskr/respberry.py
+++ flaskr/respberry.py
@@ -4,47 +4,46 @@ from flask import Flask
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
+Host = "emqx.ranzhendong.com.cn"
+Port = 1883
+Light = 35
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(Light, GPIO.OUT)
+
 app = Flask(__name__)
 app.config.from_object("config")
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-Host = "emqx.ranzhendong.com.cn"
-Port = 1883
-bedroom = 35
-ch = 26
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(bedroom, GPIO.OUT)
-GPIO.setup(ch, GPIO.OUT)
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
 
 def opens():
-    print('|      Open Light !      |')
-    print('+------------------------+')
-    GPIO.output(ch, GPIO.LOW)
+    GPIO.output(Light, GPIO.LOW)
+
 
 def close():
-    print('|      Down Light !      |')
-    print('+------------------------+')
-    GPIO.output(ch, GPIO.HIGH)
+    GPIO.output(Light, GPIO.HIGH)
+
 
 def on_connect(client, userdata, flags, rc):
-    print("emqx.eguagua.cn Connected with result code "+str(rc))
-    client.subscribe("zhendong")
+    client.subscribe("respberry")
+
 
 def on_message(client, userdata, msg):
-    code = msg.payload.decode('utf-8')
-    print("\nGet The Infomation!:\n+------------------------+")
-    if code == '开灯':
+    code = msg.payload.decode("utf-8")
+    print("\nGet The Infomation!:" + code)
+    if code == "1":
         opens()
-    elif  code == '关灯':
+    elif code == "2":
         close()
 
-if __name__ == "__main__":
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(Host, Port, 300)
-    client.loop_forever()
+
+print("emqx.ranzhendong.com.cn ready to connect ! ")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(Host, Port, 300)
+client.loop_forever()
